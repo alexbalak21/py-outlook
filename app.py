0d9352c1ee7e52958398f7fe2f24b7c9794f60f4
alexbalak21 <alex.balak@outlook.com>
2022-08-10 23:21:55 +0200

    version 0.1

--- app.py
+++ app.py
@@ -7,44 +7,49 @@ opst = pypff.open('backup.pst')
 root = opst.get_root_folder()
 
 # WHILE SUB FOLDER IS NOT 0 : GET IN ALL SUBFOLDERS AND GET MAILS
-def getAllMessages(item):
-    messages = []
-    msgs = []
-    nbrSubFolders = item.get_number_of_sub_folders()
-    while (nbrSubFolders > 0):
-        for i in nbrSubFolders:
-            currentFolder = item.get_sub_folder(i)
-            nbrMsg = currentFolder.get_number_of_sub_messages()
-            if (nbrMsg > 0):
-                for j in nbrMsg:
-                    msg = currentFolder.get_sub_message(j)
-                    messages.append(msg)
-            msgs = getAllMessages(currentFolder)
-            messages.extend(msgs)
-    return messages
+# def getAllMessages(item):
+#     messages = []
+#     msgs = []
+#     nbrSubFolders = item.get_number_of_sub_folders()
+#     while (nbrSubFolders > 0):
+#         for i in nbrSubFolders:
+#             currentFolder = item.get_sub_folder(i)
+#             nbrMsg = currentFolder.get_number_of_sub_messages()
+#             if (nbrMsg > 0):
+#                 for j in nbrMsg:
+#                     msg = currentFolder.get_sub_message(j)
+#                     messages.append(msg)
+#             msgs = getAllMessages(currentFolder)
+#             messages.extend(msgs)
+#     return messages
 
 # OPEM EACH SUB FOLDER
 def checkSubFolders(folder):
+    print ('CHECK SUB FOLDERS')
     messages = []
     nbrSubFolders = folder.get_number_of_sub_folders()
-    for n in nbrSubFolders:
+    print('NBR_SUB_FOLDERS :', nbrSubFolders)
+    for n in range(nbrSubFolders):
         currentFolder = folder.get_sub_folder(n)
-        messages.extend(getMessages(currentFolder))
+        messages.extend(goToLastSubFolder(currentFolder))
     return messages
 
 # OPEN SUB FOLDER UNTIL IT GOT NONE
 def goToLastSubFolder(folder):
+    print('GO TO LAST SUBFOLDER')
     messages = []
     messages.extend(getMessages(folder))
     gotSubFolder = True
-    while goToLastSubFolder :
+    while gotSubFolder :
         if (folder.get_number_of_sub_folders() > 0):
-            goToLastSubFolder = True
+            gotSubFolder = True
             folder = folder.get_sub_folder(0)
+            messages.extend(getMessages(folder))
+            messages.extend(checkSubFolders(folder))
         else:
-            goToLastSubFolder = False
-
-
+            gotSubFolder = False
+            break
+    return messages
 
 
 #CHECK MESSAGES
@@ -58,6 +63,10 @@ def getMessages(folder):
     else:
         return messages
     return messages
+
+
+msgs = checkSubFolders(root)
+print(len(msgs))
 
 # print(len(allMessages))
 
